b35dbac008aaf6cb5c2696c69c602ffcea3d0ddb
Dhafer Alzubairi <dx.dhafer@gmail.com>
2021-01-06 01:41:52 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from pymongo import MongoClient
+from mysql import connector
+
+
+client = MongoClient('mongodb://localhost:27017')
+db = client.bookimed.orders
+#
+# engine = create_engine(
+#     "mysql+mysqlconnector://root:3463910@localhost:3306", echo=True)
+#
+# conn = engine.connect()
+#
+# print("connected!\n connection : {}".format(conn))
+
+db.find_one()
