80e525d079389f87a62c437353f927a119bcb882
Dhafer Alzubairi <dx.dhafer@gmail.com>
2021-01-06 03:33:51 +0200

    Updated the query method to get the requested data from DB

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
+from bson import SON
 from sqlalchemy import create_engine
 from pymongo import MongoClient
 from mysql import connector
 
-
-client = MongoClient('mongodb://localhost:27017')
-# db = client.bookimed.orders
-db = client["bookimed"]["orders"]
-#
+# mySQL connection
 # engine = create_engine(
 #     "mysql+mysqlconnector://root:3463910@localhost:3306", echo=True)
 #
@@ -14,4 +11,19 @@ db = client["bookimed"]["orders"]
 #
 # print("connected!\n connection : {}".format(conn))
 
-print(db.find_one())
+# Mongo connection
+client = MongoClient('mongodb://localhost:27017')
+# All run correctly
+# 1
+# db = client.bookimed.orders
+# 2
+# db = client["bookimed"]["orders"]
+# print(db.find_one())
+# 3
+db = client["bookimed"]
+
+pipeline = [
+]
+req_data = db.orders.find_one({}, {"_id": 0, "feedbackId": 1, "flow": 1, "patient": {"name": 1}, "salaryAt": 1,
+                          "saleManager": {"firstName": 1}})
+print(req_data)
